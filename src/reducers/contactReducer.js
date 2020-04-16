6aef3e811140ef16b0f2c2570d50e93475280a76
Zach Selindh <zachselindh@gmail.com>
2020-04-16 12:51:47 -0500

    Added redux scaffolding.

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+function contactReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default contactReducer;
